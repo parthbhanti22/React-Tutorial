b86ea1475a2602fb28ff971b948f3cac91699ce5
parthbhanti22 <parthbhanti22@gmail.com>
2025-05-11 23:51:22 +0530

    Simple function based on click

--- BroCodeReact/website/my-react-app/src/App.jsx
+++ BroCodeReact/website/my-react-app/src/App.jsx
@@ -1,21 +1,9 @@
-import List from "./List.jsx"
-function App() {
-  const fruits =   [{id:1,name : "apple", calories:95},
-                    {id:2,name : "orange", calories: 45},
-                    {id:3,name : "banana" ,calories:105},
-                    {id:4,name : "coconut",calories:159},
-                    {id:5,name : "pineapple",calories:37}];
-                  
-  const vegetables =   [{id:6,name : "potatoes", calories:110},
-                        {id:7,name : "celery", calories: 15},
-                        {id:8,name : "carrots" ,calories:25},
-                        {id:9,name : "corn",calories:63},
-                        {id:10,name : "brocolli",calories:50}];
+import Button from "./Button.jsx";
 
+function App() {
   return (
     <>
-      {fruits.length>0 && <List items = {fruits} category = "Fruits"/>} 
-      {vegetables.length>0 && <List items = {vegetables} category = "Vegetables"/>}
+      <Button/>
     </>
   );
 
@@ -24,4 +12,3 @@ function App() {
 export default App
 
 
-//conditionally rendered
