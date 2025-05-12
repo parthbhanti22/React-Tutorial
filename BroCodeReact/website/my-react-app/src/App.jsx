a3b07a14d3e209ac92d2e579595177993445209f
parthbhanti22 <parthbhanti22@gmail.com>
2025-05-12 23:30:56 +0530

    Learn useState() hook,now will implement

--- BroCodeReact/website/my-react-app/src/App.jsx
+++ BroCodeReact/website/my-react-app/src/App.jsx
@@ -1,15 +1,16 @@
-//click event = An interaction when a user clicks on a specific element.
-//              We can respond to clicks by passing
-//              a callback to the OnClick event handler
+//React hook = Special function that allows finctional compnents
+//             to use react features without writing class components(React v16.8)
+//             (useState, useEffect, useContext, useReducer,useCallback, and more...)
 
-
-import Button from "./Button.jsx";
-import ProfilePicture from "./ProfilePicture.jsx";
+//useState() = A React hook that allows the creation of a stateful variable 
+//             AND a setter function to update its value in the Virtual DOM.
+//             [name,setName]
+import MyComponent from "./MyComponent.jsx";
 function App() {
   return (
     <>
+      <MyComponent/>
       
-      <ProfilePicture/>
     </>
   );
 
--- BroCodeReact/website/my-react-app/src/ProfilePicture.jsx
+++
@@ -1,9 +0,0 @@
-function ProfilePicture(){
-    const imageUrl = './src/assets/channels4_profile.jpg';
-
-    const handleClick = (e) => e.target.style.display = "none";
-
-    return(<img onClick={(e) => handleClick(e)} src = {imageUrl}></img>);
-}
-
-export default ProfilePicture
