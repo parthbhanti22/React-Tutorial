18e84b35ad9ee2f56f88bb3c30e1a6021a069cce
parthbhanti22 <parthbhanti22@gmail.com>
2025-05-13 00:01:47 +0530

    Completed my first react app myself, yayyy!!

--- BroCodeReact/website/my-react-app/src/App.jsx
+++ BroCodeReact/website/my-react-app/src/App.jsx
@@ -5,11 +5,12 @@
 //useState() = A React hook that allows the creation of a stateful variable 
 //             AND a setter function to update its value in the Virtual DOM.
 //             [name,setName]
-import MyComponent from "./MyComponent.jsx";
+import Counter from "./Counter.jsx";
+//import MyComponent from "./MyComponent.jsx";
 function App() {
   return (
     <>
-      <MyComponent/>
+      <Counter/>
       
     </>
   );
