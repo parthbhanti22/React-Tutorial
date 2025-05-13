1ff762f0fdaf7aa81b745beb17fd2e3ea3bad408
parthbhanti22 <parthbhanti22@gmail.com>
2025-05-13 23:51:17 +0530

    Updater functions

--- BroCodeReact/website/my-react-app/src/App.jsx
+++ BroCodeReact/website/my-react-app/src/App.jsx
@@ -1,14 +1,10 @@
-//onChange = event handler used primarily with form elements
-//           ex. <input>,<textarea>,<select>,<radio>
-//           Triggers a function every time the value of the input changes
-
-import ColorPicker from "./ColorPicker.jsx";
+import MyComponent from "./MyComponent.jsx";
 
 
 function App() {
   return (
     <>
-      <ColorPicker/>
+      <MyComponent/>
     </>
   );
 
---
+++ BroCodeReact/website/my-react-app/src/MyComponent.jsx
@@ -0,0 +1,39 @@
+//updater function  = A function passed as an argument to setState() usually
+//                    ex. setYear(updater function)
+//                    Allow for safe updates based on the previous state 
+//                    Used with multiple state updates and asynchronous functions
+//                    Good practice to use updater functions
+
+import React,{useState} from 'react';
+
+function MyComponent(){
+
+    const [count,setCount] = useState(0);
+
+    function increment(){
+        setCount(c => c+1);
+        setCount(c => c+1);
+        setCount(c => c+1);
+    };
+
+    function decrement(){
+        setCount(c => c-1);
+        setCount(c => c-1);
+        setCount(c => c-1);
+    };
+
+    function reset(){
+        setCount(0);
+    }
+
+    return(
+        <div>
+            <p>Count: {count}</p><br/>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={increment}>Increment</button>
+        </div>
+    );
+}
+
+export default MyComponent
