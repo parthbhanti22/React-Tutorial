e172ecafc8b94affc6a9d3f7e0000513e01c9c89
parthbhanti22 <parthbhanti22@gmail.com>
2025-05-14 00:23:29 +0530

    Update Arrays

--- BroCodeReact/website/my-react-app/src/MyComponent.jsx
+++ BroCodeReact/website/my-react-app/src/MyComponent.jsx
@@ -2,31 +2,32 @@
 import React,{useState} from 'react';
 
 function MyComponent(){
+    const[foods, setFoods] = useState(["Apple","Orange","Banana"]);
 
-    const [car,setCar] = useState({year: 2024,
-                                   make:"Ford", 
-                                   model: "Mustang"
-                                 })
+    function handleAddFood(){
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
 
-    function handleYearChange(event){
-        setCar(c => ({...c,year:event.target.value}));
+        setFoods(f => [...f,newFood]);
     }
 
-    function handleMakeChange(event){
-        setCar(c => ({...c,make:event.target.value}));
-    }
-
-    function handleModelChange(event){
-        setCar(c => ({...c,model:event.target.value}));
+    function handleRemoveFood(index){
+        setFoods(foods.filter((element,i) => i!==index));
     }
 
     return(
         <div>
-            <p>Your favourite car is : {car.year} {car.make} {car.model}</p>
-            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
-            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
-            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
-        </div>);
+            <h2>List of Food</h2>
+            <ul>
+                {foods.map((food,index) => <li key={index} onClick={() => handleRemoveFood(index)}>
+                                                {food}
+                                           </li>)}
+            </ul>
+            <input type="text" id="foodInput" placeholder="Enter food name"/>
+            <button onClick={handleAddFood}>Add Food</button><br/>
+            
+        </div>
+    );
 }
 
 export default MyComponent
