70c7ef51739be5149dfcc605cdaa389f47748119
parthbhanti22 <parthbhanti22@gmail.com>
2025-05-14 19:01:24 +0530

    Made a digital clock using Reactjs

--- BroCodeReact/website/my-react-app/src/App.jsx
+++ BroCodeReact/website/my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import MyComponent from "./MyComponent.jsx";
+import DigitalClock from "./DigitalClock.jsx";
 
 function App() {
   return (
     <>
-      <MyComponent/>
+      <DigitalClock/>
     </>
   );
 
