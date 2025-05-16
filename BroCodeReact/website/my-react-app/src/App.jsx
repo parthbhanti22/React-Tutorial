c3d17c63e896c79daf0f638624bdc228c6f1d304
parthbhanti22 <parthbhanti22@gmail.com>
2025-05-16 16:52:08 +0530

    Learnt useContext() hook

--- BroCodeReact/website/my-react-app/src/App.jsx
+++ BroCodeReact/website/my-react-app/src/App.jsx
@@ -1,9 +1,14 @@
-import DigitalClock from "./DigitalClock.jsx";
+//useContext() = React hook that allows you to share values
+//               between multiple levels of componenets
+//               without passing props through each level
 
+
+import ComponentA from "./ComponentA.jsx";
+import React from "react";
 function App() {
   return (
     <>
-      <DigitalClock/>
+      <ComponentA/>
     </>
   );
 
